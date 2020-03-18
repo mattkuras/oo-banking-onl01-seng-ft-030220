2b186fab958a978a646770cb8b3408d25d4ff748
Matt Kuras <mattkuras@gmail.com>
2020-03-18 20:20:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,6 +26,8 @@ class Transfer
 end 
 
   def reverse_transfer
+    if @status == "reversed"
+      
     self.sender.balance += amount
     self.receiver.balance -= amount
     self.status = "reversed"
